c79a48d3c7d4ac433c2019623f3b6480a06ffef7
jhuang429 <jhuang9240@gmail.com>
2020-03-16 13:42:18 -0400

    1:42 commit

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -3,14 +3,29 @@ import SushiContainer from './containers/SushiContainer';
 import Table from './containers/Table';
 
 // Endpoint!
+
 const API = "http://localhost:3000/sushis"
 
 class App extends Component {
 
+  
+  state = {
+    sushis: [],
+    sushiIndex:0,
+  }
+
+  showMore = () => {
+    this.setState({sushiIndex: this.state.sushiIndex+= 4})
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:3000/sushis").then(resp=>resp.json()).then(cont => this.setState({sushis: cont}))
+  }
+
   render() {
     return (
       <div className="app">
-        <SushiContainer />
+        <SushiContainer sushis={this.state.sushis} index={this.state.sushiIndex}/>
         <Table />
       </div>
     );
--- sushi-saga-client/src/containers/SushiContainer.js
+++ sushi-saga-client/src/containers/SushiContainer.js
@@ -1,14 +1,17 @@
 import React, { Fragment } from 'react'
 import MoreButton from '../components/MoreButton'
+import Sushi from '../components/Sushi'
 
 const SushiContainer = (props) => {
+
+  let index = props.index
+  let range = 
+
   return (
     <Fragment>
       <div className="belt">
         {
-          /* 
-             Render Sushi components here!
-          */
+          props.sushis[props.index].map(sushi => <Sushi sushi={sushi}/>)
         }
         <MoreButton />
       </div>
